232a1b6a8afa70973e5c8866676f348f2e02bd50
RotiChapati <rotichapati102k@gmail.com>
2022-02-26 22:42:21 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const connectDB = require('../config/db')
 const { urlencoded } = require('body-parser')
 const authRoute = require('./routes/auth.js')
 const cors = require("cors")
-
+const path = require("path")
 const app = express()
 
 // Database connected
@@ -13,7 +13,11 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 app.use(express.json({ extended: false }))
-
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(authRoute)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server started on ${port}...`))
